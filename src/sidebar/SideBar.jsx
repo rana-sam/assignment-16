58d04e460144c6cab89953b1112bb5b7cbdee8a9
Rana Sam <ranausama2082001@gmail.com>
2022-03-22 21:02:17 +0500

    Update SideBar.jsx

--- src/sidebar/SideBar.jsx
+++ src/sidebar/SideBar.jsx
@@ -2,6 +2,21 @@ import React from 'react'
 
 export default function SideBar() {
   return (
-    <div>SideBar</div>
+    <div>
+        <div className="container">
+            <div className="row">
+                <div className="col-3">
+                    <li>My Day</li>
+                    <li>Important</li>
+                    <li>Planned</li>
+                    <li>Assignment to me</li>
+                    <li>flaged Email</li>
+                    <li>tasks</li>
+                </div>
+                <div className="col-3"></div>
+                <div className="col-3"></div>
+            </div>
+        </div>
+    </div>
   )
 }
